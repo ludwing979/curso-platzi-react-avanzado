adaa42a8977997b45fbc7ac52fa2e89a7a7ca3ec
ludwing979 <magoludwing@gmail.com>
2021-04-08 17:18:36 -0500

    Se agrega configuracion de babel y se termina la configuracion de webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,36 @@
+const path = require('path')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  entry: './src/index.js',
   output: {
-    filename: 'app.bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /.html$/,
+        use: {
+          loader: 'html-loader',
+        }
+      },
+      {
+        test: /\.(js|jsx)/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
   },
   plugins: [
-    new htmlWebpackPlugin()
-  ]
+    new htmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html'
+    })
+  ],
 }
