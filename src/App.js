4ecb0bd1d08ea12e873100b89f77d0f85ec618c4
ludwing979 <magoludwing@gmail.com>
2021-04-09 12:19:44 -0500

    Adding global styles and creation of component listofcategories

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
-import { Category } from './components/Category'
+import { ListOfCategories } from './components/ListOfCategories'
+
+import { GlobalStyles } from './GlobalStyles'
 
 export const App = () => (
-  <Category />
+  <>
+    <GlobalStyles />
+    <ListOfCategories />
+  </>
 )
