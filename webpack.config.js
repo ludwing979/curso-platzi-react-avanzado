670359ff21745f8b92843f22912eb749d25a9c82
ludwing979 <magoludwing@gmail.com>
2021-04-09 12:28:03 -0500

    Using information close to real

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
         }
       },
       {
-        test: /\.(js|jsx)/,
+        test: /\.(js$|jsx$)/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
