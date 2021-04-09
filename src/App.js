4ebef89e04f03e47fe9ad82771bd99ada27426a3
ludwing979 <magoludwing@gmail.com>
2021-04-09 14:22:10 -0500

    adding logo component to the page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 import { GlobalStyles } from './GlobalStyles'
 
 export const App = () => (
   <>
     <GlobalStyles />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </>
