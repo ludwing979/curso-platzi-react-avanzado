69cf1b1b279d538b66a8bfab01ee8fb374ee611a
ludwing979 <magoludwing@gmail.com>
2021-04-09 14:00:06 -0500

    Creation of components of PhotoCard and ListOfPhotoCard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { ListOfCategories } from './components/ListOfCategories'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 
 import { GlobalStyles } from './GlobalStyles'
 
@@ -8,5 +9,6 @@ export const App = () => (
   <>
     <GlobalStyles />
     <ListOfCategories />
+    <ListOfPhotoCards />
   </>
 )
---
+++ src/components/PhotoCard/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { MdFavoriteBorder } from 'react-icons/md'
+
+import { Button, Img, ImgWrapper } from './styles'
+
+const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
+
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  return (
+    <article>
+      <a href={`/details/${id}`}>
+        <ImgWrapper>
+          <Img src={src} />
+        </ImgWrapper>
+      </a>
+
+      <Button>
+        <MdFavoriteBorder size='32px' /> {likes} likes!
+      </Button>
+    </article>
+  )
+}
