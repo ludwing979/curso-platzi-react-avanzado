0c6258fd0e3ab5da90df3eda5628895af43c8979
ludwing979 <magoludwing@gmail.com>
2021-04-14 13:25:33 -0500

    Using mutations to change the number of likes and save it in the DB

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 
-import { Button, Img, ImgWrapper, Article } from './styles'
+import { Img, ImgWrapper, Article } from './styles'
+
+import { FavButton } from '../FavButton'
+import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
@@ -11,8 +13,6 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
-
-  const Icon = liked ? MdFavorite : MdFavoriteBorder
 
   return (
     <Article ref={element}>
@@ -24,10 +24,21 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
                 <Img src={src} />
               </ImgWrapper>
             </a>
-
-            <Button onClick={() => setLiked(!liked)}>
-              <Icon size='32px' /> {likes} likes!
-            </Button>
+            <ToggleLikeMutation>
+              {
+                (toggleLike) => {
+                  const handleFavClick = () => {
+                    !liked && toggleLike({
+                      variables: {
+                        input: { id }
+                      }
+                    })
+                    setLiked(!liked)
+                  }
+                  return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
+                }
+              }
+            </ToggleLikeMutation>
           </>
       }
 
