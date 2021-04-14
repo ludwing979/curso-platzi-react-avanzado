a9e6e0665db92fb176992030b2f935acc154de7b
ludwing979 <magoludwing@gmail.com>
2021-04-14 12:59:37 -0500

    Refactoring and using loading and error for the querys

--- src/container/PhotoCardWithQuery.js
+++ src/container/PhotoCardWithQuery.js
@@ -3,27 +3,32 @@ import { gql } from 'apollo-boost'
 import { Query } from 'react-apollo'
 
 import { PhotoCard } from '../components/PhotoCard'
+import { Spinner } from '../components/Spinner'
 
-const query = gql`
-  query getPhotos($categoryId: ID) {
-    photos(categoryId: $categoryId) {
+const GET_SINGLE_PHOTO = gql`
+  query getSinglePhoto($id:ID!) {
+    photo(id:$id) {
       id
       categoryId
       src
       likes
-      liked
       userId
+      liked
     }
   }
 `
 
+const renderProp = ({ loading, error, data = {} }) => {
+  if (loading) return <Spinner />
+  if (error) return <p>Error!</p>
+
+  const { photo = {} } = data
+  console.log(data)
+  return <PhotoCard {...photo} />
+}
+
 export const PhotoCardWithQuery = ({ id }) => (
-  <Query query={query} variables={{ id }}>
-    {
-      ({ loading, error, data = {} }) => {
-        const { photo = {} } = data
-        return <PhotoCard {...photo} />
-      }
-    }
+  <Query query={GET_SINGLE_PHOTO} variables={{ id }}>
+    {renderProp}
   </Query>
 )
