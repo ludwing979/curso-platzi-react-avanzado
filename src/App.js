f0df64b87fb4e3bb5432816aa6841ac01ba0c762
ludwing979 <magoludwing@gmail.com>
2021-04-15 12:01:17 -0500

    Creation of pages for user, favs and notRegisterUser

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import { Router } from '@reach/router'
 
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisterUser } from './pages/NotRegisterUser'
 import { Logo } from './components/Logo'
 import { NavBar } from './components/NavBar'
 
 import { GlobalStyles } from './styles/GlobalStyles'
 
+const UserLogged = ({ children }) => {
+  return children({ isAuth: true })
+}
 export const App = () => {
   return (
     <>
@@ -18,6 +24,20 @@ export const App = () => {
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+                </Router>
+              : <Router>
+                <NotRegisterUser path='/favs' />
+                <NotRegisterUser path='/user' />
+                </Router>
+        }
+      </UserLogged>
       <NavBar />
     </>
   )
---
+++ src/pages/NotRegisterUser.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NotRegisterUser = () => {
+  return (
+    <div>
+      NotRegisterUser
+    </div>
+  )
+}
