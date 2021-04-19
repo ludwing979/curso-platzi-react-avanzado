813caa3579a5b8ea9f737b272ef45c536d82553e
ludwing979 <magoludwing@gmail.com>
2021-04-19 18:07:37 -0500

    Management fo the loading and error when you register a user

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 import { useInputValue } from '../../hooks/useInputValue'
 
-import { Form, Input, Button, Title } from './styles'
+import { Form, Input, Button, Title, Error } from './styles'
 
-export const UserForm = ({ onSubmit, title }) => {
+export const UserForm = ({ onSubmit, title, error, disabled }) => {
   const email = useInputValue('')
   const password = useInputValue('')
 
@@ -15,12 +15,13 @@ export const UserForm = ({ onSubmit, title }) => {
 
   return (
     <>
-      <Title>{title}</Title>
-      <Form onSubmit={handleSubmit}>
-        <Input type='text' placeholder='Email' {...email} />
-        <Input type='password' placeholder='Password' {...password} />
-        <Button>{title}</Button>
+      <Form disabled={disabled} onSubmit={handleSubmit}>
+        <Title>{title}</Title>
+        <Input disabled={disabled} type='text' placeholder='Email' {...email} />
+        <Input disabled={disabled} type='password' placeholder='Password' {...password} />
+        <Button disabled={disabled}>{title}</Button>
       </Form>
+      {error && <Error>{error}</Error>}
     </>
   )
 }
