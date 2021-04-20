e4b6ca3a0ee8217f5238d3211a9098821e215d8a
ludwing979 <magoludwing@gmail.com>
2021-04-20 12:14:28 -0500

    Using apollo to save the token and use it in every request

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from '@reach/router'
-import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 
 import { Img, ImgWrapper, Article } from './styles'
@@ -10,10 +9,8 @@ import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
-  const key = `like-${id}`
-  const [liked, setLiked] = useLocalStorage(key, false)
 
   return (
     <Article ref={element}>
@@ -29,12 +26,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               {
                 (toggleLike) => {
                   const handleFavClick = () => {
-                    !liked && toggleLike({
+                    toggleLike({
                       variables: {
                         input: { id }
                       }
                     })
-                    setLiked(!liked)
                   }
                   return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
                 }
