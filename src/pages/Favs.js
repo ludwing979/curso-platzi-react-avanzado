d51034b8453ba0dfd36d8ae833d4d193361612e7
ludwing979 <magoludwing@gmail.com>
2021-04-20 17:46:31 -0500

    Learning the use of react helmet for changing the head of the page

--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Layout } from '../components/Layout'
 
 import { FavsWithQuery } from '../container/GetFavorites'
 
 export const Favs = () => {
   return (
-    <>
+    <Layout title='Tus Favoritos' subtitle='Aqui puedes encontrar tus favoritos'>
       <div>Favs</div>
       <FavsWithQuery />
-    </>
+    </Layout>
   )
 }
