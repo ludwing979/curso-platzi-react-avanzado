3443686f7c3af7c78eb08b767c2031bb0e5f85c9
ludwing979 <magoludwing@gmail.com>
2021-04-20 17:16:07 -0500

    Last configurations for the routes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
-import React from 'react'
-import { Router } from '@reach/router'
-import Context from './Context'
+import React, { useContext } from 'react'
+import { Redirect, Router } from '@reach/router'
+import { Context } from './Context'
 
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
-import { NotRegisterUser } from './pages/NotRegisterUser'
 import { Logo } from './components/Logo'
+import { NotFound } from './pages/NotFound'
 import { NavBar } from './components/NavBar'
+import { NotRegisterUser } from './pages/NotRegisterUser'
 
 import { GlobalStyles } from './styles/GlobalStyles'
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
   return (
     <>
       <GlobalStyles />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path='/' />
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisterUser path='/login' />}
+        {!isAuth && <Redirect noThrow from='/favs' to='/login' />}
+        {!isAuth && <Redirect noThrow from='/user' to='/login' />}
+        {isAuth && <Redirect noThrow from='/login' to='/' />}
+        <Favs path='/favs' />
+        <User path='/user' />
       </Router>
-      <Context.Consumer>
-        {
-          ({ isAuth }) =>
-            isAuth
-              ? <Router>
-                <Favs path='/favs' />
-                <User path='/user' />
-              </Router>
-              : <Router>
-                <NotRegisterUser path='/favs' />
-                <NotRegisterUser path='/user' />
-              </Router>
-        }
-      </Context.Consumer>
       <NavBar />
     </>
   )
