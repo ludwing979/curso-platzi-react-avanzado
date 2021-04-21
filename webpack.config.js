33e02d3773003fc9f0a5afdef566e069f9a1d1b2
ludwing979 <magoludwing@gmail.com>
2021-04-21 17:53:39 -0500

    Last details to make the page PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -35,16 +36,43 @@ module.exports = {
       filename: 'index.html'
     }),
     new WebpackPwaManifestPlugin({
-      name: 'Petgram - Tu app de fotos de mascotas',
-      shortname: 'Petgram 🐶',
-      description: 'Con Petgram puedes encontrar fotos de animales domesticosmuy facilmente',
-      background_color: '#fff',
-      theme_color: '#b1a',
+      filename: 'manifest.webmanifest',
+      name: 'IntaPlatzi',
+      description: 'Tu app preferida para encontrar esas mascotas que tanto te encantan',
+      orientation: 'portrait',
+      display: 'standalone',
+      start_url: '/',
+      scope: '/',
+      background_color: '#456BD9',
+      theme_color: '#456BD9',
       icons: [
         {
-          src: path.resolve('src/assets/icon.png'),
+          src: path.resolve(__dirname, 'src/assets/icon.png'),
           sizes: [96, 128, 192, 256, 384, 512],
-          purpose: 'maskable'
+          type: 'image/png',
+          purpose: 'any maskable'
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: 'service-worker.js',
+      clientsClaim: true,
+      skipWaiting: true,
+      maximumFileSizeToCacheInBytes: 5000000,
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com|images.unsplash.com)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://petgram-server-ludwing.vercel.app'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
         }
       ]
     })
