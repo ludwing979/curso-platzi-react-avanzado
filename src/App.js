e341971db55ae0f2c9b3fa40c032fc9a7adcdbaf
ludwing979 <magoludwing@gmail.com>
2021-04-21 16:06:42 -0500

    Learning to use react.lazy and suspense

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useContext } from 'react'
+import React, { useContext, Suspense } from 'react'
 import { Redirect, Router } from '@reach/router'
 import { Context } from './Context'
 
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
-import { Favs } from './pages/Favs'
+// import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { Logo } from './components/Logo'
 import { NotFound } from './pages/NotFound'
@@ -13,10 +13,12 @@ import { NotRegisterUser } from './pages/NotRegisterUser'
 
 import { GlobalStyles } from './styles/GlobalStyles'
 
+const Favs = React.lazy(() => import('./pages/Favs'))
+
 export const App = () => {
   const { isAuth } = useContext(Context)
   return (
-    <>
+    <Suspense fallback={<div />}>
       <GlobalStyles />
       <Logo />
       <Router>
@@ -32,6 +34,6 @@ export const App = () => {
         <User path='/user' />
       </Router>
       <NavBar />
-    </>
+    </Suspense>
   )
 }
